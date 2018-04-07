4cfb04251d1ab5cf257905c4e3db5d3b5f8fc6b4
Salvador <scoronel@belatrixsf.com>
2018-04-07 13:14:01 -0500

    First commit

---
+++ FrameworkForNS/MyVisanet.swift
@@ -0,0 +1,16 @@
+//
+//  MyVisanet.swift
+//  FrameworkForNS
+//
+//  Created by Salvador on 4/7/18.
+//  Copyright © 2018 Salvador. All rights reserved.
+//
+
+import Foundation
+
+public class MyVisanet {
+    private init() { }
+    public static func openVisanet() -> String {
+        return "You're calling method openVisanet()....";
+    }
+}
