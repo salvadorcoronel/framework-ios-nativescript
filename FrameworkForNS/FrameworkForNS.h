4074fc8e5b7697de0704a47fc82728034f1f8e5f
Salvador <scoronel@belatrixsf.com>
2018-04-07 09:40:49 -0500

    Initial Commit

---
+++ FrameworkForNS/FrameworkForNS.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkForNS.h
+//  FrameworkForNS
+//
+//  Created by Salvador on 4/7/18.
+//  Copyright © 2018 Salvador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkForNS.
+FOUNDATION_EXPORT double FrameworkForNSVersionNumber;
+
+//! Project version string for FrameworkForNS.
+FOUNDATION_EXPORT const unsigned char FrameworkForNSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkForNS/PublicHeader.h>
+
+
